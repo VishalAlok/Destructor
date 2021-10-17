2d2799979e9151606d07bbd05afeb80771a512bb
vishal.Alok <you@example.com>
2021-10-17 22:24:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Desctructor
+{
+    class User
+    {
+        public User()
+        {
+            Console.WriteLine("An Instance of class created");
+        }
+        // Destructor
+        ~User()
+        {
+            Console.WriteLine("An Instance of class destroyed");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Details();
+            GC.Collect();
+        }
+        public static void Details()
+        {
+            // Created instance of the class
+            User user = new User();
+        }
+    }
+}
